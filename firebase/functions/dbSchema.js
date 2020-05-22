d6302a3216edbb4e3d3ecdf4c603a488a73988ca
Valery <valery.ptv@gmail.com>
2020-05-22 19:16:37 +0300

    added get user details, notifications routes

--- firebase/functions/dbSchema.js
+++ firebase/functions/dbSchema.js
@@ -10,35 +10,45 @@ let db = {
   ],
   comments: [
     {
-      body: "nice scream!",
-      createdAt: "2020-04-18T11:49:33.628Z",
-      screamId: " EgJwZZd8oY09rGLSmXYB",
-      userHandle: "new"
+      body: 'nice scream!',
+      createdAt: '2020-04-18T11:49:33.628Z',
+      screamId: 'EgJwZZd8oY09rGLSmXYB',
+      userHandle: 'new'
+    }
+  ],
+  notifications: [
+    {
+      recipient: 'user',
+      sender: 'john',
+      read: 'true | false',
+      screamId: 'asdjkaskdjsakdsakd',
+      type: 'like | comment',
+      createdAt: '2020-04-18T11:49:33.628Z'
     }
   ],
   users: [
     {
-      userId: "Lzajd5K3k2NmsTwrno3ugMN97Oo1",
-      email: "test18@g.com",
-      handle: "new",
-      createdAt: "2020-04-16T07:15:18.403Z",
-      imageUrl: "https://firebasestorage.googleapis.com/v0/b/socialape228.appspot.com/o/undefined?alt=media",
-      bio: "my bio hello",
-      website: "http://oogle.com",
-      location: "LA, Ca"
+      userId: 'Lzajd5K3k2NmsTwrno3ugMN97Oo1',
+      email: 'test18@g.com',
+      handle: 'new',
+      createdAt: '2020-04-16T07:15:18.403Z',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/socialape228.appspot.com/o/undefined?alt=media',
+      bio: 'my bio hello',
+      website: 'http://oogle.com',
+      location: 'LA, Ca'
     }
   ],
   userDetails: {
     // Redux data
     credentials: {
-      userId: "Lzajd5K3k2NmsTwrno3ugMN97Oo1",
-      email: "test18@g.com",
-      handle: "new",
-      createdAt: "2020-04-16T07:15:18.403Z",
-      imageUrl: "https://firebasestorage.googleapis.com/v0/b/socialape228.appspot.com/o/undefined?alt=media",
-      bio: "my bio hello",
-      website: "http://oogle.com",
-      location: "LA, Ca"
+      userId: 'Lzajd5K3k2NmsTwrno3ugMN97Oo1',
+      email: 'test18@g.com',
+      handle: 'new',
+      createdAt: '2020-04-16T07:15:18.403Z',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/socialape228.appspot.com/o/undefined?alt=media',
+      bio: 'my bio hello',
+      website: 'http://oogle.com',
+      location: 'LA, Ca'
     },
     likes: []
   }
--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,10 @@
 const functions = require('firebase-functions');
 const express = require('express');
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser, getUserDetails, markNotificationsRead } = require('./handlers/users')
 const { getAllScreams, postOneScream, getScream, deleteScream, commentOnScream, likeScream, unlikeScream } = require('./handlers/screams')
 const FBAuth = require('./util/FBAuth');
+
+const {db} = require('./util/admin');
 
 const app = express();
 
@@ -14,15 +16,74 @@ app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 app.get('/scream/:screamId/like', FBAuth, likeScream);
 app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 app.delete('/scream/:screamId', FBAuth, deleteScream)
-// TODO like a scream
-// TODO unlike a scream
-// TODO comment on scream
 
-// users route
+// users routes
 app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications', FBAuth, markNotificationsRead)
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+  .region('europe-west1')
+  .firestore
+  .document('/likes/{id}')
+  .onCreate(snapshot => {
+    db.doc(`/screams/${snapshot.data().screamId}`).get()
+      .then(doc => {
+        // eslint-disable-next-line promise/always-return
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  })
+
+exports.deleteNotificationOnUnLike = functions
+  .region('europe-west1')
+  .firestore
+  .document('/likes/{id}')
+  .onDelete(snapshot => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .catch(err => {
+        console.error(err);
+      })
+  })
+
+exports.createNotificationOnComment = functions
+  .region('europe-west1')
+  .firestore
+  .document('/comments/{id}')
+  .onCreate(snapshot => {
+    db.doc(`/screams/${snapshot.data().screamId}`).get()
+      .then(doc => {
+        // eslint-disable-next-line promise/always-return
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'comment',
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  })
